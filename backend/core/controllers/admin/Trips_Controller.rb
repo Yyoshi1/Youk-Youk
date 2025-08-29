4f0c138601b8d527bda3a72efd1e637078c290bc
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-30 00:09:14 +0100

    Create Trips_Controller.rb

---
+++ backend/core/controllers/admin/Trips_Controller.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class Admin::TripsController < ApplicationController
+  before_action :set_trip, only: [:show, :update, :destroy]
+
+  def index
+    @trips = Trip.all
+    render json: @trips
+  end
+
+  def show
+    render json: @trip
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      render json: @trip, status: :created
+    else
+      render json: @trip.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @trip.update(trip_params)
+      render json: @trip
+    else
+      render json: @trip.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @trip.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_trip
+    @trip = Trip.find(params[:id])
+  end
+
+  def trip_params
+    params.require(:trip).permit(:name, :start_location, :end_location, :departure_time, :arrival_time, :country_id, :model_id)
+  end
+end
