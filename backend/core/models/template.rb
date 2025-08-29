59c8d3ab0afec5c35d5abbb2395b6dfe252a9c7c
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-30 00:48:37 +0100

    Update template.rb

--- backend/core/models/template.rb
+++ backend/core/models/template.rb
@@ -2,9 +2,5 @@
 
 class Template < ApplicationRecord
   has_many :template_versions, dependent: :destroy
-  has_many :template_components, through: :template_versions
-  has_many :template_assets, through: :template_components
-
   validates :name, presence: true, uniqueness: true
-  validates :description, presence: true
 end
