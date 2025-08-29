8f1b94f3d6a1e90c9fc6a826f5a241da1a1a1bc3
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-30 00:55:44 +0100

    Update templates_Controller.rb

--- backend/core/controllers/admin/templates_Controller.rb
+++ backend/core/controllers/admin/templates_Controller.rb
@@ -1,35 +1,53 @@
 # frozen_string_literal: true
 
-class CreateTemplates < ActiveRecord::Migration[7.0]
-  def change
-    # جدول القوالب
-    create_table :templates do |t|
-      t.string :name, null: false
-      t.text :description, null: false
-      t.timestamps
-    end
-    add_index :templates, :name, unique: true
+module Admin
+  class TemplatesController < ApplicationController
+    before_action :set_template, only: [:show, :edit, :update, :destroy]
 
-    # جدول نسخ القوالب
-    create_table :template_versions do |t|
-      t.references :template, null: false, foreign_key: true
-      t.string :version_number, null: false
-      t.timestamps
+    def index
+      @templates = Template.all
     end
 
-    # جدول مكونات النسخ
-    create_table :template_components do |t|
-      t.references :template_version, null: false, foreign_key: true
-      t.string :name, null: false
-      t.string :component_type, null: false
-      t.timestamps
+    def show
     end
 
-    # جدول ملفات النسخ
-    create_table :template_assets do |t|
-      t.references :template_version, null: false, foreign_key: true
-      t.string :file_name
-      t.timestamps
+    def new
+      @template = Template.new
+    end
+
+    def create
+      @template = Template.new(template_params)
+      if @template.save
+        redirect_to admin_template_path(@template), notice: 'Template created successfully.'
+      else
+        render :new
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if @template.update(template_params)
+        redirect_to admin_template_path(@template), notice: 'Template updated successfully.'
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @template.destroy
+      redirect_to admin_templates_path, notice: 'Template deleted successfully.'
+    end
+
+    private
+
+    def set_template
+      @template = Template.find(params[:id])
+    end
+
+    def template_params
+      params.require(:template).permit(:name)
     end
   end
 end
