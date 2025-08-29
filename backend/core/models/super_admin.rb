cb8d544eacf87b916221f6ced6bbf0928c6cd331
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-29 18:15:41 +0100

    Create super_admin.rb

---
+++ backend/core/models/super_admin.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class SuperAdmin < ActiveRecord::Base
+  has_many :continent_admins
+  has_many :assistant_admins, as: :creator
+  has_many :investors, as: :creator
+end
