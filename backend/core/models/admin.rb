26a43acf9e66087086af8c66e13b13c1dfb7522a
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-29 23:56:40 +0100

    Update admin.rb

--- backend/core/models/admin.rb
+++ backend/core/models/admin.rb
@@ -4,11 +4,10 @@ class Admin < ApplicationRecord
   belongs_to :continent, optional: true
   belongs_to :country, optional: true
   belongs_to :model, optional: true
+  belongs_to :super_admin, class_name: 'Admin', optional: true
 
-  validates :name, :role, :email, presence: true
+  has_many :sub_admins, class_name: 'Admin', foreign_key: 'super_admin_id', dependent: :destroy
+
+  validates :name, :email, presence: true
   validates :email, uniqueness: true
-
-  # المساعدين يمكن لكل مسؤول إنشاءهم حسب صلاحياته
-  has_many :assistants, class_name: 'Admin', foreign_key: 'supervisor_id', dependent: :destroy
-  belongs_to :supervisor, class_name: 'Admin', optional: true
 end
