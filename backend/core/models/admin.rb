271196872c9b22936c03d87b9e0119c18a311b7a
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-29 22:17:29 +0100

    Update admin.rb

--- backend/core/models/admin.rb
+++ backend/core/models/admin.rb
@@ -1,24 +1,14 @@
 # frozen_string_literal: true
 
 class Admin < ApplicationRecord
-  belongs_to :super_admin, optional: true
   belongs_to :continent, optional: true
   belongs_to :country, optional: true
   belongs_to :model, optional: true
 
-  # مسؤول يمكنه إضافة مساعدين
-  has_many :assistants, class_name: "Admin", foreign_key: "super_admin_id"
-  
-  # مسؤول يمكنه إدارة المستثمرين ضمن مستواه
-  has_many :investors, ->(admin) {
-    if admin.model
-      where(model: admin.model)
-    elsif admin.country
-      where(country: admin.country)
-    elsif admin.continent
-      where(continent: admin.continent)
-    else
-      all
-    end
-  }, class_name: "Investor"
+  validates :name, :role, :email, presence: true
+  validates :email, uniqueness: true
+
+  # المساعدين يمكن لكل مسؤول إنشاءهم حسب صلاحياته
+  has_many :assistants, class_name: 'Admin', foreign_key: 'supervisor_id', dependent: :destroy
+  belongs_to :supervisor, class_name: 'Admin', optional: true
 end
