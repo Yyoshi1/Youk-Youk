c41a6ae474193b7c8035c32f487c16ddee36c6e4
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-29 21:39:19 +0100

    Update driver.rb

--- backend/core/models/driver.rb
+++ backend/core/models/driver.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 class Driver < ApplicationRecord
-  # ارتباط بالرحلات
+  belongs_to :model
+  belongs_to :country
+
+  # الارتباط بالرحلات
   has_many :trip_drivers
-  has_many :trips, through: :trip_drivers
+  has_many :trips, through: :trip_drivers, dependent: :destroy
 
   # التحقق من البيانات الأساسية
-  validates :name, :email, :license_number, presence: true
-  validates :email, uniqueness: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone, presence: true, uniqueness: true
+  validates :license_number, presence: true, uniqueness: true
 end
