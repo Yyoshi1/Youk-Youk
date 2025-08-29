ebd09a5d735938fc6b47bff077595df7893416d2
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-29 21:41:03 +0100

    Update country.rb

--- backend/core/models/country.rb
+++ backend/core/models/country.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 class Country < ApplicationRecord
-  self.table_name = 'countries'
-
   belongs_to :continent
   has_many :models, dependent: :destroy
-  has_many :country_admins, dependent: :destroy
+  has_many :investors, dependent: :nullify
 
   validates :name, presence: true, uniqueness: { scope: :continent_id }
 end
