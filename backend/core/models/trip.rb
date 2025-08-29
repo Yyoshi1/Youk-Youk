bb387502e7c4ab05cf30007ad53a703fe2d75503
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-29 21:39:57 +0100

    Update trip.rb

--- backend/core/models/trip.rb
+++ backend/core/models/trip.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
 class Trip < ApplicationRecord
-  # الارتباط بالنماذج، الدول، والمسؤولين
-  belongs_to :model, optional: true
-  belongs_to :country, optional: true
-  belongs_to :continent, optional: true
+  belongs_to :model
+  belongs_to :country
 
-  # الارتباط بالركاب والسائقين
   has_many :trip_passengers
   has_many :passengers, through: :trip_passengers
+
   has_many :trip_drivers
   has_many :drivers, through: :trip_drivers
 
-  # التحقق من الحقول الأساسية
-  validates :start_location, :end_location, :departure_time, presence: true
+  # التحقق من البيانات الأساسية
+  validates :start_location, :end_location, :departure_time, :arrival_time, presence: true
 end
