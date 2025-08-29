3eb01e8d7eaba8dca76b43981f5470417ceef6e8
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-30 00:06:40 +0100

    Create country_Admins_Controller.rb

---
+++ backend/core/controllers/admin/country_Admins_Controller.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class Admin::CountryAdminsController < ApplicationController
+  before_action :set_admin, only: [:show, :update, :destroy]
+
+  def index
+    @admins = Admin.where.not(country_id: nil).where(continent_id: params[:continent_id])
+    render json: @admins
+  end
+
+  def show
+    render json: @admin
+  end
+
+  def create
+    @admin = Admin.new(admin_params)
+    if @admin.save
+      render json: @admin, status: :created
+    else
+      render json: @admin.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @admin.update(admin_params)
+      render json: @admin
+    else
+      render json: @admin.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @admin.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+
+  def admin_params
+    params.require(:admin).permit(:name, :email, :continent_id, :country_id, :super_admin_id)
+  end
+end
