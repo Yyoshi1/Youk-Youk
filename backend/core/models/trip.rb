60b059f4157fe9e7bb82951fb91ebde14d2096f3
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-30 00:00:17 +0100

    Update trip.rb

--- backend/core/models/trip.rb
+++ backend/core/models/trip.rb
@@ -2,14 +2,10 @@
 
 class Trip < ApplicationRecord
   belongs_to :model
-  belongs_to :country
-
-  has_many :trip_passengers
+  has_many :trip_passengers, dependent: :destroy
   has_many :passengers, through: :trip_passengers
-
-  has_many :trip_drivers
+  has_many :trip_drivers, dependent: :destroy
   has_many :drivers, through: :trip_drivers
 
-  # التحقق من البيانات الأساسية
-  validates :start_location, :end_location, :departure_time, :arrival_time, presence: true
+  validates :name, :start_location, :end_location, presence: true
 end
