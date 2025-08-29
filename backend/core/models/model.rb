74e20bcc7cc3fd486f8f638a0f73c6b2b9e92706
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-30 00:44:23 +0100

    Update model.rb

--- backend/core/models/model.rb
+++ backend/core/models/model.rb
@@ -2,10 +2,9 @@
 
 class Model < ApplicationRecord
   belongs_to :country
-  has_many :investors, dependent: :nullify
-
-  # مسؤول النموذج والمساعدين
+  has_many :trips, dependent: :destroy
   has_many :model_admins, class_name: 'Admin', foreign_key: 'model_id', dependent: :destroy
+  has_many :investors, dependent: :nullify
 
   validates :name, presence: true, uniqueness: { scope: :country_id }
 end
