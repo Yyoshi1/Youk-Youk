a64d5c60e5289ab31ffbe478285c8dc0388e30a3
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-29 18:55:10 +0100

    Create 006_create_investors.rb

--- backend/db/migrate/006_create_investors.rb
+++ backend/db/migrate/006_create_investors.rb
@@ -1,12 +1,16 @@
+# frozen_string_literal: true
+
 class CreateInvestors < ActiveRecord::Migration[7.0]
   def change
     create_table :investors do |t|
       t.string :name, null: false
-      t.string :email, null: false, index: { unique: true }
-      t.integer :investor_type, null: false
-      t.references :creator, polymorphic: true, null: false
-      t.references :level_admin, polymorphic: true, null: false
-
+      t.string :email, null: false, unique: true
+      t.string :phone_number
+      t.string :investor_type, null: false # full_level or partial_share
+      t.references :continent, foreign_key: true
+      t.references :country, foreign_key: true
+      t.references :model, foreign_key: true
+      t.decimal :share_percentage, precision: 5, scale: 2, default: 100.0
       t.timestamps
     end
   end
