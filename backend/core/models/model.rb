75ad67ab6675c077f4d384bc4d1ba61280653e5c
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-29 21:41:36 +0100

    Update model.rb

--- backend/core/models/model.rb
+++ backend/core/models/model.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class Model < ApplicationRecord
-  self.table_name = 'models'
-
   belongs_to :country
-  has_many :model_admins, dependent: :destroy
+  has_many :drivers, dependent: :destroy
+  has_many :passengers, dependent: :destroy
   has_many :trips, dependent: :destroy
+  has_many :investors, dependent: :nullify
 
   validates :name, presence: true, uniqueness: { scope: :country_id }
 end
