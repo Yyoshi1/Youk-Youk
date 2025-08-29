d7f8e7f51dca38c01430bdf35aed78844fb4a1e1
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-29 22:11:18 +0100

    Update continent.rb

--- backend/core/models/continent.rb
+++ backend/core/models/continent.rb
@@ -4,5 +4,8 @@ class Continent < ApplicationRecord
   has_many :countries, dependent: :destroy
   has_many :investors, dependent: :nullify
 
+  # مسؤول القارة والمساعدين
+  has_many :continent_admins, class_name: 'Admin', foreign_key: 'continent_id', dependent: :destroy
+
   validates :name, presence: true, uniqueness: true
 end
