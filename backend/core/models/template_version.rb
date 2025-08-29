1f292d51390aebfe56c6c40677e581eee6d4c2f5
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-30 00:25:50 +0100

    Update template_version.rb

--- backend/core/models/template_version.rb
+++ backend/core/models/template_version.rb
@@ -3,10 +3,6 @@
 class TemplateVersion < ApplicationRecord
   belongs_to :template
   has_many :template_components, dependent: :destroy
-  has_many :template_assets, dependent: :destroy
 
-  accepts_nested_attributes_for :template_components, allow_destroy: true
-  accepts_nested_attributes_for :template_assets, allow_destroy: true
-
-  validates :version_number, presence: true
+  validates :version_number, presence: true, uniqueness: { scope: :template_id }
 end
