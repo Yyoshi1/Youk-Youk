b1f9fd3e32602a6d4d0c796415795d2c202934a8
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-30 00:49:50 +0100

    Update template_version.rb

--- backend/core/models/template_version.rb
+++ backend/core/models/template_version.rb
@@ -3,6 +3,5 @@
 class TemplateVersion < ApplicationRecord
   belongs_to :template
   has_many :template_components, dependent: :destroy
-
-  validates :version_number, presence: true, uniqueness: { scope: :template_id }
+  validates :version_number, presence: true
 end
