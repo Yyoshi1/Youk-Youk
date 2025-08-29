8de5b91f74c78a0e6da6a8030a421411b05e117d
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-30 00:22:53 +0100

    Update templates_controller.rb

--- backend/core/controllers/api/v1/templates_controller.rb
+++ backend/core/controllers/api/v1/templates_controller.rb
@@ -3,21 +3,52 @@
 module Api
   module V1
     class TemplatesController < ApplicationController
-      before_action :set_template, only: [:show]
+      before_action :set_template, only: %i[show update destroy]
 
+      # GET /api/v1/templates
       def index
         @templates = Template.all
         render json: @templates
       end
 
+      # GET /api/v1/templates/:id
       def show
         render json: @template
+      end
+
+      # POST /api/v1/templates
+      def create
+        @template = Template.new(template_params)
+        if @template.save
+          render json: @template, status: :created
+        else
+          render json: { errors: @template.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /api/v1/templates/:id
+      def update
+        if @template.update(template_params)
+          render json: @template
+        else
+          render json: { errors: @template.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /api/v1/templates/:id
+      def destroy
+        @template.destroy
+        head :no_content
       end
 
       private
 
       def set_template
         @template = Template.find(params[:id])
+      end
+
+      def template_params
+        params.require(:template).permit(:name, :description)
       end
     end
   end
