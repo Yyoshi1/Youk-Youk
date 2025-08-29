29c0a989a1cc354064eacabd715a7ea58510c5e6
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-30 00:43:43 +0100

    Update country.rb

--- backend/core/models/country.rb
+++ backend/core/models/country.rb
@@ -4,8 +4,6 @@ class Country < ApplicationRecord
   belongs_to :continent
   has_many :models, dependent: :destroy
   has_many :investors, dependent: :nullify
-
-  # مسؤول الدولة والمساعدين
   has_many :country_admins, class_name: 'Admin', foreign_key: 'country_id', dependent: :destroy
 
   validates :name, presence: true, uniqueness: { scope: :continent_id }
