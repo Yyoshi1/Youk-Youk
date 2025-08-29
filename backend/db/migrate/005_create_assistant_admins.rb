1da332fd535e639e532cf51cfd3b141b51ee7671
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-29 18:50:26 +0100

    Create 005_create_assistant_admins.rb

--- backend/db/migrate/005_create_assistant_admins.rb
+++ backend/db/migrate/005_create_assistant_admins.rb
@@ -1,13 +1,13 @@
-class CreateAssistantAdmins < ActiveRecord::Migration[7.0]
-  def change
-    create_table :assistant_admins do |t|
-      t.string :name, null: false
-      t.string :email, null: false, index: { unique: true }
-      t.string :encrypted_password, null: false
-      t.integer :level_type, null: false
-      t.references :creator, polymorphic: true, null: false
-      t.references :level_admin, polymorphic: true, null: false
+# frozen_string_literal: true
 
+class CreateInvestors < ActiveRecord::Migration[7.0]
+  def change
+    create_table :investors do |t|
+      t.string :name, null: false
+      t.string :email, null: false, unique: true
+      t.string :investment_type, null: false # full_level, share_level
+      t.references :level_reference, polymorphic: true, null: false # continent/country/model
+      t.decimal :share_percentage, default: 100
       t.timestamps
     end
   end
