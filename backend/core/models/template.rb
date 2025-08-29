bca0907b9649d5d34e7d28bb7e8be40f171bbf2b
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-30 00:15:47 +0100

    Update template.rb

--- backend/core/models/template.rb
+++ backend/core/models/template.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
+
 class Template < ApplicationRecord
-  has_many :versions, class_name: 'TemplateVersion', dependent: :destroy
-  validates :country_code, presence: true, uniqueness: true
-  # owner could be investor or system
-  belongs_to :owner, polymorphic: true, optional: true
+  has_many :template_versions, dependent: :destroy
+  has_many :template_components, through: :template_versions
+  has_many :template_assets, through: :template_versions
+
+  accepts_nested_attributes_for :template_versions, allow_destroy: true
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
 end
