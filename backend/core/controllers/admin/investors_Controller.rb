d259e8c24650c88a8eb165a127c9865f8c366a0d
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-30 00:56:27 +0100

    Update investors_Controller.rb

--- backend/core/controllers/admin/investors_Controller.rb
+++ backend/core/controllers/admin/investors_Controller.rb
@@ -1,52 +1,53 @@
 # frozen_string_literal: true
 
-class Admin::InvestorsController < ApplicationController
-  before_action :set_investor, only: [:show, :update, :destroy, :detach_level]
+module Admin
+  class InvestorsController < ApplicationController
+    before_action :set_investor, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @investors = Investor.all
-    render json: @investors
-  end
-
-  def show
-    render json: @investor
-  end
-
-  def create
-    @investor = Investor.new(investor_params)
-    if @investor.save
-      render json: @investor, status: :created
-    else
-      render json: @investor.errors, status: :unprocessable_entity
+    def index
+      @investors = Investor.all
     end
-  end
 
-  def update
-    if @investor.update(investor_params)
-      render json: @investor
-    else
-      render json: @investor.errors, status: :unprocessable_entity
+    def show
     end
-  end
 
-  def destroy
-    @investor.destroy
-    head :no_content
-  end
+    def new
+      @investor = Investor.new
+    end
 
-  # فصل المستوى عند بيع الحصة بالكامل
-  def detach_level
-    @investor.detach_level_if_full_sale
-    render json: { message: 'تم فصل المستوى بنجاح' }
-  end
+    def create
+      @investor = Investor.new(investor_params)
+      if @investor.save
+        redirect_to admin_investor_path(@investor), notice: 'Investor created successfully.'
+      else
+        render :new
+      end
+    end
 
-  private
+    def edit
+    end
 
-  def set_investor
-    @investor = Investor.find(params[:id])
-  end
+    def update
+      if @investor.update(investor_params)
+        redirect_to admin_investor_path(@investor), notice: 'Investor updated successfully.'
+      else
+        render :edit
+      end
+    end
 
-  def investor_params
-    params.require(:investor).permit(:name, :email, :investor_type, :share_percentage, :continent_id, :country_id, :model_id)
+    def destroy
+      @investor.destroy
+      redirect_to admin_investors_path, notice: 'Investor deleted successfully.'
+    end
+
+    private
+
+    def set_investor
+      @investor = Investor.find(params[:id])
+    end
+
+    def investor_params
+      params.require(:investor).permit(:name, :email, :investor_type, :share_percentage, :continent_id, :country_id, :model_id)
+    end
   end
 end
