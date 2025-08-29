079132c93ed42f6b53b7290f116e7b69bad4f994
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-30 00:54:53 +0100

    Update templates_controller.rb

--- backend/core/controllers/api/v1/templates_controller.rb
+++ backend/core/controllers/api/v1/templates_controller.rb
@@ -3,39 +3,34 @@
 module Api
   module V1
     class TemplatesController < ApplicationController
-      before_action :set_template, only: %i[show update destroy]
+      before_action :set_template, only: [:show, :update, :destroy]
 
-      # GET /api/v1/templates
       def index
         @templates = Template.all
         render json: @templates
       end
 
-      # GET /api/v1/templates/:id
       def show
-        render json: @template
+        render json: @template, include: { template_versions: { include: :template_components } }
       end
 
-      # POST /api/v1/templates
       def create
         @template = Template.new(template_params)
         if @template.save
           render json: @template, status: :created
         else
-          render json: { errors: @template.errors.full_messages }, status: :unprocessable_entity
+          render json: @template.errors, status: :unprocessable_entity
         end
       end
 
-      # PATCH/PUT /api/v1/templates/:id
       def update
         if @template.update(template_params)
           render json: @template
         else
-          render json: { errors: @template.errors.full_messages }, status: :unprocessable_entity
+          render json: @template.errors, status: :unprocessable_entity
         end
       end
 
-      # DELETE /api/v1/templates/:id
       def destroy
         @template.destroy
         head :no_content
@@ -48,7 +43,7 @@ module Api
       end
 
       def template_params
-        params.require(:template).permit(:name, :description)
+        params.require(:template).permit(:name)
       end
     end
   end
