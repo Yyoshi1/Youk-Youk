4f8477194b59d81e6e3fd33f7821067bc379233a
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-30 00:38:07 +0100

    Update application.rb

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -1,11 +1,27 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 require 'rails/all'
+
 Bundler.require(*Rails.groups)
 
 module Youkyouk
   class Application < Rails::Application
     config.load_defaults 7.1
+
+    # اللغة الافتراضية
+    config.i18n.default_locale = :en
+
+    # تفعيل إعدادات الوقت العالمي
     config.time_zone = 'UTC'
-    config.active_record.schema_format = :sql
+
+    # إعدادات النوافذ الموحدة لجميع المسؤولين
+    config.windows_ui = true
+
+    # إعدادات توليد Docs تلقائي
+    config.auto_docs = true
+
+    # إعدادات حماية كود المستثمرين
+    config.investor_protection = true
   end
 end
