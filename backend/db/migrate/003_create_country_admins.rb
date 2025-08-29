caa22ce49ee0b82174e4541ee419a2478bd07cd3
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-29 18:49:00 +0100

    Create 003_create_country_admins.rb

--- backend/db/migrate/003_create_country_admins.rb
+++ backend/db/migrate/003_create_country_admins.rb
@@ -1,11 +1,10 @@
-class CreateCountryAdmins < ActiveRecord::Migration[7.0]
-  def change
-    create_table :country_admins do |t|
-      t.string :name, null: false
-      t.string :email, null: false, index: { unique: true }
-      t.references :continent_admin, null: false, foreign_key: true
-      t.string :encrypted_password, null: false
+# frozen_string_literal: true
 
+class CreateModels < ActiveRecord::Migration[7.0]
+  def change
+    create_table :models do |t|
+      t.string :name, null: false
+      t.references :country, null: false, foreign_key: true
       t.timestamps
     end
   end
