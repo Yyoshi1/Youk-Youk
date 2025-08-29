65257d2caec5b6885cb94a38348046752169302b
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-30 00:47:09 +0100

    Update driver.rb

--- backend/core/models/driver.rb
+++ backend/core/models/driver.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 class Driver < ApplicationRecord
-  has_many :trip_drivers
-  has_many :trips, through: :trip_drivers
-
+  has_many :trips
   validates :name, :email, presence: true
   validates :email, uniqueness: true
 end
