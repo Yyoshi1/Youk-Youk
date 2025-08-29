5e7d311b025469eb67d17e149535759eda5146eb
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-29 21:19:22 +0100

    Update passenger.rb

--- backend/core/models/passenger.rb
+++ backend/core/models/passenger.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 class Passenger < ApplicationRecord
-  # ارتباط بالرحلات
+  belongs_to :model
+  belongs_to :country
+
+  # الارتباط بالرحلات
+  has_many :trips, foreign_key: :passenger_id, dependent: :destroy
   has_many :trip_passengers
   has_many :trips, through: :trip_passengers
 
   # التحقق من البيانات الأساسية
-  validates :name, :email, presence: true
-  validates :email, uniqueness: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone, presence: true, uniqueness: true
 end
