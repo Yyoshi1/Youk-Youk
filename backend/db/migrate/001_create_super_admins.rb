f6bc9258c687c738f0b289d88ffc91d3587bb3d2
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-29 18:20:37 +0100

    Create 001_create_super_admins.rb

---
+++ backend/db/migrate/001_create_super_admins.rb
@@ -0,0 +1,11 @@
+class CreateSuperAdmins < ActiveRecord::Migration[7.0]
+  def change
+    create_table :super_admins do |t|
+      t.string :name, null: false
+      t.string :email, null: false, index: { unique: true }
+      t.string :encrypted_password, null: false
+
+      t.timestamps
+    end
+  end
+end
