549f08d72608b54d5f4872f61e26be29909eb887
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-09 02:37:57 +0100

    Create Sidebar.tsx

---
+++ frontend/admin/src/components/Sidebar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export const Sidebar: React.FC = () => {
+  return (
+    <div className="w-64 bg-gray-900 text-white flex flex-col">
+      <h1 className="text-2xl font-bold p-4">Youkyouk Admin</h1>
+      <nav className="flex-1 flex flex-col">
+        <NavLink className="p-4 hover:bg-gray-700" to="/countries">Countries</NavLink>
+        <NavLink className="p-4 hover:bg-gray-700" to="/users-trips">Users & Trips</NavLink>
+        <NavLink className="p-4 hover:bg-gray-700" to="/modules">Modules</NavLink>
+        <NavLink className="p-4 hover:bg-gray-700" to="/analytics">Analytics</NavLink>
+        <NavLink className="p-4 hover:bg-gray-700" to="/settings">Settings</NavLink>
+      </nav>
+    </div>
+  );
+};
