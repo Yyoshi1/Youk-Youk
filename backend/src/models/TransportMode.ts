fed58ab17a3f185e2a3b6819a80c46b0627fd000
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-09 02:46:02 +0100

    Create TransportMode.ts

---
+++ backend/src/models/TransportMode.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class TransportMode {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ default: true })
+  active: boolean; // يمكن تعطيله بدون حذفه
+}
