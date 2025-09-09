aa15af6404b29edc92bbfa3e6796e197fd518ab4
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-09 13:54:24 +0100

    Update TransportMode.ts

--- backend/src/models/TransportMode.ts
+++ backend/src/models/TransportMode.ts
@@ -1,13 +1,25 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from "typeorm";
+import { Driver } from "./Driver";
+import { Ride } from "./Ride";
 
+/**
+ * TransportMode entity represents a vehicle or transport type.
+ * - Examples: car, small truck, large truck, electric bike
+ */
 @Entity()
 export class TransportMode {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
-  name: string;
+  type: string; // car, small truck, large truck, electric bike
 
-  @Column({ default: true })
-  active: boolean; // يمكن تعطيله بدون حذفه
+  @Column()
+  imageUrl: string; // vehicle image
+
+  @ManyToOne(() => Driver, (driver) => driver.vehicles)
+  driver: Driver;
+
+  @OneToMany(() => Ride, (ride) => ride.transportMode)
+  rides: Ride[];
 }
