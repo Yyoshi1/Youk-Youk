cacd9d00553c69918f01cdbf09da4d94b6f4d868
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-09 02:32:02 +0100

    Update Country.ts

--- backend/src/models/Country.ts
+++ backend/src/models/Country.ts
@@ -1,28 +1,25 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
 import { User } from "./User";
 import { Trip } from "./Trip";
-import { ModuleSetting } from "./ModuleSetting";
+import { Module } from "./Module";
 
 @Entity()
 export class Country {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ unique: true })
+  @Column()
   name: string;
 
   @Column()
-  defaultLanguage: string;
+  language: string;
 
-  @Column()
-  currency: string;
-
-  @OneToMany(() => User, (user) => user.country)
+  @OneToMany(() => User, user => user.country)
   users: User[];
 
-  @OneToMany(() => Trip, (trip) => trip.country)
+  @OneToMany(() => Trip, trip => trip.country)
   trips: Trip[];
 
-  @OneToMany(() => ModuleSetting, (module) => module.country)
-  moduleSettings: ModuleSetting[];
+  @OneToMany(() => Module, module => module.country)
+  modules: Module[];
 }
