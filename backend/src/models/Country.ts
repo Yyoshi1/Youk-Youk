4912b28777ffc0bb272417b5feffe8a03d74b85a
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-09 13:39:13 +0100

    Update Country.ts

--- backend/src/models/Country.ts
+++ backend/src/models/Country.ts
@@ -1,5 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
-import { Module } from "./Module";
+import { Vehicle } from "./Vehicle";
+import { User } from "./User";
 import { Ride } from "./Ride";
 
 @Entity()
@@ -8,16 +9,19 @@ export class Country {
   id: number;
 
   @Column({ type: "varchar", length: 100 })
-  name: string;
+  name: string; // : Morocco, France, USA
 
-  @Column({ type: "varchar", length: 10 })
-  currency: string;
+  @Column({ type: "varchar", length: 10, default: "MAD" })
+  currency: string; // 
 
-  @Column({ type: "varchar", length: 10 })
-  defaultLanguage: string;
+  @Column({ type: "varchar", length: 10, default: "fr" })
+  defaultLanguage: string; // 
 
-  @OneToMany(() => Module, (module) => module.country)
-  modules: Module[];
+  @OneToMany(() => Vehicle, (vehicle) => vehicle.country)
+  vehicles: Vehicle[];
+
+  @OneToMany(() => User, (user) => user.country)
+  users: User[];
 
   @OneToMany(() => Ride, (ride) => ride.country)
   rides: Ride[];
