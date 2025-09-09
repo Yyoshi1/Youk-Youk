6e2e95d5808fc16ab21580f5c7bc3b04bf150034
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-09 13:49:00 +0100

    Update Country.ts

--- backend/src/models/Country.ts
+++ backend/src/models/Country.ts
@@ -1,28 +1,111 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
-import { Vehicle } from "./Vehicle";
-import { User } from "./User";
-import { Ride } from "./Ride";
+import { DataTypes, Model, Optional } from "sequelize";
+import sequelize from "../config/database";
 
-@Entity()
-export class Country {
-  @PrimaryGeneratedColumn()
-  id: number;
+// Import other models for relations
+import User from "./User";
+import Driver from "./Driver";
+import Ride from "./Ride";
+import Vehicle from "./Vehicle";
+import Module from "./Module";
 
-  @Column({ type: "varchar", length: 100 })
-  name: string; // : Morocco, France, USA
-
-  @Column({ type: "varchar", length: 10, default: "MAD" })
-  currency: string; // 
-
-  @Column({ type: "varchar", length: 10, default: "fr" })
-  defaultLanguage: string; // 
-
-  @OneToMany(() => Vehicle, (vehicle) => vehicle.country)
-  vehicles: Vehicle[];
-
-  @OneToMany(() => User, (user) => user.country)
-  users: User[];
-
-  @OneToMany(() => Ride, (ride) => ride.country)
-  rides: Ride[];
+// ------------------------------
+// Interface for Country attributes
+// ------------------------------
+interface CountryAttributes {
+  id: number;                 // Unique ID for the country
+  name: string;               // Country name
+  code: string;               // ISO country code
+  defaultLanguage: string;    // Default language for the country
+  defaultCurrency: string;    // Default currency
+  createdAt?: Date;           // Created timestamp
+  updatedAt?: Date;           // Updated timestamp
 }
+
+// Fields optional during creation
+interface CountryCreationAttributes extends Optional<CountryAttributes, "id"> {}
+
+// ------------------------------
+// Country model definition
+// ------------------------------
+class Country extends Model<CountryAttributes, CountryCreationAttributes> implements CountryAttributes {
+  public id!: number;
+  public name!: string;
+  public code!: string;
+  public defaultLanguage!: string;
+  public defaultCurrency!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+
+  // ------------------------------
+  // Relations with other models
+  // ------------------------------
+  public getUsers!: () => Promise<User[]>;
+  public getDrivers!: () => Promise<Driver[]>;
+  public getRides!: () => Promise<Ride[]>;
+  public getVehicles!: () => Promise<Vehicle[]>;
+  public getModules!: () => Promise<Module[]>;
+}
+
+// ------------------------------
+// Initialize Country model
+// ------------------------------
+Country.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    code: {
+      type: DataTypes.STRING(3),
+      allowNull: false,
+      unique: true,
+    },
+    defaultLanguage: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+      defaultValue: "en",
+    },
+    defaultCurrency: {
+      type: DataTypes.STRING(5),
+      allowNull: false,
+      defaultValue: "USD",
+    },
+  },
+  {
+    sequelize,
+    tableName: "countries",
+    modelName: "Country",
+  }
+);
+
+// ===============================
+// Define associations / relationships
+// ===============================
+
+// A country has many users
+Country.hasMany(User, { foreignKey: "countryId", as: "users" });
+User.belongsTo(Country, { foreignKey: "countryId", as: "country" });
+
+// A country has many drivers
+Country.hasMany(Driver, { foreignKey: "countryId", as: "drivers" });
+Driver.belongsTo(Country, { foreignKey: "countryId", as: "country" });
+
+// A country has many rides
+Country.hasMany(Ride, { foreignKey: "countryId", as: "rides" });
+Ride.belongsTo(Country, { foreignKey: "countryId", as: "country" });
+
+// A country has many vehicles
+Country.hasMany(Vehicle, { foreignKey: "countryId", as: "vehicles" });
+Vehicle.belongsTo(Country, { foreignKey: "countryId", as: "country" });
+
+// A country has many modules (features/plugins)
+Country.hasMany(Module, { foreignKey: "countryId", as: "modules" });
+Module.belongsTo(Country, { foreignKey: "countryId", as: "country" });
+
+export default Country;
